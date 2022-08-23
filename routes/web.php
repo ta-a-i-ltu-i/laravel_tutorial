89df111e9e24dcb33f6c474dd3f890e78dc08966
ta-a-i-ltu-i <chatter.boy33@gmail.com>
2022-08-23 09:10:44 +0900

    詳細画面まで作成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,13 @@ Route::get('/', function () {
 });
 
 Route::get('/tests/test', [TestController::class,'index']);
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function () {
+    Route::get('/index', [ContactFormController::class,'index'])->name('contact.index');
+    Route::get('/create', [ContactFormController::class,'create'])->name('contact.create');
+    Route::post('/store', [ContactFormController::class,'store'])->name('contact.store');
+    Route::get('/show/{id}', [ContactFormController::class,'show'])->name('contact.show');
+});
 
 Auth::routes();
 
